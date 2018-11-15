cb7ba2ff31d506ed55199ce7fa28c6648305ffeb
John <pls@gooby.co>
2018-11-15 01:18:59 -0500

    added the ability to modify all different currencies

--- D3Edit.py
+++ D3Edit.py
@@ -8,7 +8,6 @@ from os.path import isfile, isdir, join
 from binascii import hexlify as _hexlify
 
 # pip3 install protobuf
-import Hero_pb2
 import Account_pb2
 
 BYTE_MAX_VALUE = 255
@@ -81,10 +80,29 @@ def encrypt_save(data: (bytes, bytearray)) -> (bytes, bytearray):
 if __name__ == "__main__":
     # parse arguments
     parser = ArgumentParser(description="A script to encrypt/decrypt and modify Diablo III saves")
-    parser.add_argument("-i", "--in-file", type=str, required=True, help="The save file you want to work with")
-    parser.add_argument("-o", "--out-file", type=str, help="The save file you want to output to")
+    parser.add_argument("-i", "--in-file", type=str, required=True, help="The account file you want to work with")
+    parser.add_argument("-o", "--out-file", type=str, help="The account file you want to output to")
     mod_group = parser.add_argument_group("modifications")
     mod_group.add_argument("--gold", type=int, help="The amount of gold you want your characters to have")
+    mod_group.add_argument("--blood-shards", type=int, help="The amount of blood shards you want your characters to have")
+    mod_group.add_argument("--platinum", type=int, help="The amount of platinum you want your characters to have")
+    mod_group.add_argument("--reusable-parts", type=int, help="The amount of reusable parts you want your characters to have")
+    mod_group.add_argument("--arcane-dust", type=int, help="The amount of arcane dust you want your characters to have")
+    mod_group.add_argument("--veiled-crystals", type=int, help="The amount of veiled crystals you want your characters to have")
+    mod_group.add_argument("--deaths-breath", type=int, help="The amount of deaths breath you want your characters to have")
+    mod_group.add_argument("--forgotten-souls", type=int, help="The amount of forgotten souls you want your characters to have")
+    mod_group.add_argument("--khanduran-runes", type=int, help="The amount of Khanduran runes you want your characters to have")
+    mod_group.add_argument("--caldeum-nightshade", type=int, help="The amount of Caldeum nightshade you want your characters to have")
+    mod_group.add_argument("--arreat-war-tapestries", type=int, help="The amount of Arret War tapestries you want your characters to have")
+    mod_group.add_argument("--corrupted-angel-flesh", type=int, help="The amount of corrupted angel flesh you want your characters to have")
+    mod_group.add_argument("--westmarch-holy-water", type=int, help="The amount of Westmarch holy water you want your characters to have")
+    mod_group.add_argument("--demon-organ-diablo", type=int, help="The amount of Diablo demon organs you want your characters to have")
+    mod_group.add_argument("--demon-organ-ghom", type=int, help="The amount of Ghom demon organs you want your characters to have")
+    mod_group.add_argument("--demon-organ-siege-breaker", type=int, help="The amount of siege breaker demon organs you want your characters to have")
+    mod_group.add_argument("--demon-organ-skeleton-king", type=int, help="The amount of skeleton king demon organs you want your characters to have")
+    mod_group.add_argument("--demon-organ-eyes", type=int, help="The amount of eye demon organs you want your characters to have")
+    mod_group.add_argument("--demon-organ-spinal-cords", type=int, help="The amount of spinal cord demon organs you want your characters to have")
+    mod_group.add_argument("--demon-organ-teeth", type=int, help="The amount of teeth demon organs you want your characters to have")
     args = parser.parse_args()
 
     # make sure the input file exists
@@ -122,13 +140,111 @@ if __name__ == "__main__":
             else:
                 print("Unknown currency ID: %s" % (currency.id))
     # modify account here
-    if args.gold:
-        for i in range(len(asd.partitions)):
-            #for j in range(len(asd.partitions[i].currency_data.currency)):
-            # asd.partitions[j].currency_data.currency[j].id
-            if len(asd.partitions[i].currency_data.currency) > 0:
-                asd.partitions[i].currency_data.currency[0].count = args.gold
-                print("Set slot %s gold to %s" % (i, args.gold))
+    for i in range(len(asd.partitions)):
+        #for j in range(len(asd.partitions[i].currency_data.currency)):
+        # asd.partitions[j].currency_data.currency[j].id
+        if len(asd.partitions[i].currency_data.currency) > 0:
+            # gold
+            if args.gold:
+                if len(asd.partitions[i].currency_data.currency) >= 1:
+                    asd.partitions[i].currency_data.currency[0].count = args.gold
+                    print("Set slot %s gold to %s" % (i, args.gold))
+            # blood shards
+            if args.blood_shards:
+                if len(asd.partitions[i].currency_data.currency) >= 2:
+                    asd.partitions[i].currency_data.currency[1].count = args.blood_shards
+                    print("Set slot %s blood shards to %s" % (i, args.blood_shards))
+            # platinum
+            if args.platinum:
+                if len(asd.partitions[i].currency_data.currency) >= 3:
+                    asd.partitions[i].currency_data.currency[2].count = args.platinum
+                    print("Set slot %s platinum to %s" % (i, args.platinum))
+            # reusable parts
+            if args.reusable_parts:
+                if len(asd.partitions[i].currency_data.currency) >= 4:
+                    asd.partitions[i].currency_data.currency[3].count = args.reusable_parts
+                    print("Set slot %s reusable parts to %s" % (i, args.reusable_parts))
+            # arcane dust
+            if args.arcane_dust:
+                if len(asd.partitions[i].currency_data.currency) >= 5:
+                    asd.partitions[i].currency_data.currency[4].count = args.arcane_dust
+                    print("Set slot %s arcane dust to %s" % (i, args.arcane_dust))
+            # veiled crystals
+            if args.veiled_crystals:
+                if len(asd.partitions[i].currency_data.currency) >= 6:
+                    asd.partitions[i].currency_data.currency[5].count = args.veiled_crystals
+                    print("Set slot %s veiled crystals to %s" % (i, args.veiled_crystals))
+            # deaths breath
+            if args.deaths_breath:
+                if len(asd.partitions[i].currency_data.currency) >= 7:
+                    asd.partitions[i].currency_data.currency[6].count = args.deaths_breath
+                    print("Set slot %s deaths breath to %s" % (i, args.deaths_breath))
+            # forgotten souls
+            if args.forgotten_souls:
+                if len(asd.partitions[i].currency_data.currency) >= 8:
+                    asd.partitions[i].currency_data.currency[7].count = args.forgotten_souls
+                    print("Set slot %s forgotten souls to %s" % (i, args.forgotten_souls))
+            # khanduran runes
+            if args.khanduran_runes:
+                if len(asd.partitions[i].currency_data.currency) >= 9:
+                    asd.partitions[i].currency_data.currency[8].count = args.khanduran_runes
+                    print("Set slot %s Khanduran runes to %s" % (i, args.khanduran_runes))
+            # caldeum nightshade
+            if args.caldeum_nightshade:
+                if len(asd.partitions[i].currency_data.currency) >= 10:
+                    asd.partitions[i].currency_data.currency[9].count = args.caldeum_nightshade
+                    print("Set slot %s Caldeum nightshade to %s" % (i, args.caldeum_nightshade))
+            # arreat war tapestries
+            if args.arreat_war_tapestries:
+                if len(asd.partitions[i].currency_data.currency) >= 11:
+                    asd.partitions[i].currency_data.currency[10].count = args.veiled_crystals
+                    print("Set slot %s Arreat War tapestries to %s" % (i, args.veiled_crystals))
+            # corrupted angel flesh
+            if args.corrupted_angel_flesh:
+                if len(asd.partitions[i].currency_data.currency) >= 12:
+                    asd.partitions[i].currency_data.currency[11].count = args.corrupted_angel_flesh
+                    print("Set slot %s corrupted angel flesh to %s" % (i, args.corrupted_angel_flesh))
+            # westmarch holy water
+            if args.westmarch_holy_water:
+                if len(asd.partitions[i].currency_data.currency) >= 13:
+                    asd.partitions[i].currency_data.currency[12].count = args.westmarch_holy_water
+                    print("Set slot %s Westmarch holy water to %s" % (i, args.westmarch_holy_water))
+            # demon organ - diablo
+            if args.demon_organ_diablo:
+                if len(asd.partitions[i].currency_data.currency) >= 14:
+                    asd.partitions[i].currency_data.currency[13].count = args.demon_organ_diablo
+                    print("Set slot %s Diablo demon organs to %s" % (i, args.demon_organ_diablo))
+            # demon organ - ghom
+            if args.demon_organ_ghom:
+                if len(asd.partitions[i].currency_data.currency) >= 15:
+                    asd.partitions[i].currency_data.currency[14].count = args.demon_organ_ghom
+                    print("Set slot %s Ghom demon organs to %s" % (i, args.demon_organ_ghom))
+            # demon organ - siege breaker
+            if args.demon_organ_siege_breaker:
+                if len(asd.partitions[i].currency_data.currency) >= 16:
+                    asd.partitions[i].currency_data.currency[15].count = args.demon_organ_siege_breaker
+                    print("Set slot %s siege breaker demon organs to %s" % (i, args.demon_organ_siege_breaker))
+            # demon organ - skeleton king
+            if args.demon_organ_skeleton_king:
+                if len(asd.partitions[i].currency_data.currency) >= 17:
+                    asd.partitions[i].currency_data.currency[16].count = args.demon_organ_skeleton_king
+                    print("Set slot %s skeleton king demon organs to %s" % (i, args.demon_organ_skeleton_king))
+            # demon organ - eye
+            if args.demon_organ_eyes:
+                if len(asd.partitions[i].currency_data.currency) >= 18:
+                    asd.partitions[i].currency_data.currency[17].count = args.demon_organ_eyes
+                    print("Set slot %s eye demon organs to %s" % (i, args.demon_organ_eyes))
+            # demon organ - spinal cord
+            if args.demon_organ_spinal_cords:
+                if len(asd.partitions[i].currency_data.currency) >= 19:
+                    asd.partitions[i].currency_data.currency[18].count = args.demon_organ_spinal_cords
+                    print("Set slot %s spinal cord demon organs to %s" % (i, args.demon_organ_spinal_cords))
+            # demon organ - teeth
+            if args.demon_organ_teeth:
+                if len(asd.partitions[i].currency_data.currency) >= 20:
+                    asd.partitions[i].currency_data.currency[19].count = args.demon_organ_teeth
+                    print("Set slot %s teeth demon organs to %s" % (i, args.demon_organ_teeth))
+
     # end account modifications
     account_mod_dec = asd.SerializeToString()
     account_mod_enc = encrypt_save(account_mod_dec)
@@ -137,26 +253,3 @@ if __name__ == "__main__":
         with open(args.out_file, "wb") as f:
             f.write(account_mod_enc)
     # end account output
-
-    # hero ID is in big endian for god knows why (maybe PS3 and Xbox 360 releases?)
-    last_hero_save = hexlify(pack(">Q", asd.digest.last_played_hero_id.id_low))
-
-    if isdir(join("saves", "Modded", HERO_DIR)):
-        last_hero_save_path = join("saves", "Modded", HERO_DIR, last_hero_save + ".dat")
-        if isfile(last_hero_save_path):
-            with open(last_hero_save_path, "rb") as f:
-                hero_enc = f.read()
-            hero_dec = decrypt_save(hero_enc)
-            hsd = Hero_pb2.SavedDefinition()
-            hsd.ParseFromString(hero_dec)
-            #print(hsd.items.items)
-            for single in hsd.items.items:
-                for affix in single.generator.base_affixes:
-                    if str(affix) in affix_list:
-                        print(affix_list[str(affix)])
-                    else:
-                        print("Unknown Affix: %s" % (affix))
-            # modify hero here
-            hero_mod_dec = hsd.SerializeToString()
-            hero_mod_enc = encrypt_save(hero_mod_dec)
-            # output the modified hero
--- README.md
+++ README.md
@@ -8,19 +8,73 @@ An example of how to mod gold would work like this (this is the only thing imple
 `python D3Edit.py -i account.dat -o account_mod.dat --gold 999999999`
 
 ```
-usage: main.py [-h] -i IN_FILE [-o OUT_FILE] [--gold GOLD]
+usage: D3Edit.py [-h] -i IN_FILE [-o OUT_FILE] [--gold GOLD]
+                 [--blood-shards BLOOD_SHARDS] [--platinum PLATINUM]
+                 [--reusable-parts REUSABLE_PARTS] [--arcane-dust ARCANE_DUST]
+                 [--veiled-crystals VEILED_CRYSTALS]
+                 [--deaths-breath DEATHS_BREATH]
+                 [--forgotten-souls FORGOTTEN_SOULS]
+                 [--khanduran-runes KHANDURAN_RUNES]
+                 [--caldeum-nightshade CALDEUM_NIGHTSHADE]
+                 [--arreat-war-tapestries ARREAT_WAR_TAPESTRIES]
+                 [--corrupted-angel-flesh CORRUPTED_ANGEL_FLESH]
+                 [--westmarch-holy-water WESTMARCH_HOLY_WATER]
+                 [--demon-organ-diablo DEMON_ORGAN_DIABLO]
+                 [--demon-organ-ghom DEMON_ORGAN_GHOM]
+                 [--demon-organ-siege-breaker DEMON_ORGAN_SIEGE_BREAKER]
+                 [--demon-organ-skeleton-king DEMON_ORGAN_SKELETON_KING]
+                 [--demon-organ-eye DEMON_ORGAN_EYE]
+                 [--demon-organ-spinal-cord DEMON_ORGAN_SPINAL_CORD]
+                 [--demon-organ-teeth DEMON_ORGAN_TEETH]
 
 A script to encrypt/decrypt and modify Diablo III saves
 
 optional arguments:
   -h, --help            show this help message and exit
   -i IN_FILE, --in-file IN_FILE
-                        The save file you want to work with
+                        The account file you want to work with
   -o OUT_FILE, --out-file OUT_FILE
-                        The save file you want to output to
+                        The account file you want to output to
 
 modifications:
   --gold GOLD           The amount of gold you want your characters to have
+  --blood-shards BLOOD_SHARDS
+                        The amount of blood shards you want your characters to have
+  --platinum PLATINUM   The amount of platinum you want your characters to have
+  --reusable-parts REUSABLE_PARTS
+                        The amount of reusable parts you want your characters to have
+  --arcane-dust ARCANE_DUST
+                        The amount of arcane dust you want your characters to have
+  --veiled-crystals VEILED_CRYSTALS
+                        The amount of veiled crystals you want your characters to have
+  --deaths-breath DEATHS_BREATH
+                        The amount of deaths breath you want your characters to have
+  --forgotten-souls FORGOTTEN_SOULS
+                        The amount of forgotten souls you want your characters to have
+  --khanduran-runes KHANDURAN_RUNES
+                        The amount of Khanduran runes you want your characters to have
+  --caldeum-nightshade CALDEUM_NIGHTSHADE
+                        The amount of Caldeum nightshade you want your characters to have
+  --arreat-war-tapestries ARREAT_WAR_TAPESTRIES
+                        The amount of Arret War tapestries you want your characters to have
+  --corrupted-angel-flesh CORRUPTED_ANGEL_FLESH
+                        The amount of corrupted angel flesh you want your characters to have
+  --westmarch-holy-water WESTMARCH_HOLY_WATER
+                        The amount of Westmarch holy water you want your characters to have
+  --demon-organ-diablo DEMON_ORGAN_DIABLO
+                        The amount of Diablo demon organs you want your characters to have
+  --demon-organ-ghom DEMON_ORGAN_GHOM
+                        The amount of Ghom demon organs you want your characters to have
+  --demon-organ-siege-breaker DEMON_ORGAN_SIEGE_BREAKER
+                        The amount of siege breaker demon organs you want your characters to have
+  --demon-organ-skeleton-king DEMON_ORGAN_SKELETON_KING
+                        The amount of skeleton king demon organs you want your characters to have
+  --demon-organ-eye DEMON_ORGAN_EYE
+                        The amount of eye demon organs you want your characters to have
+  --demon-organ-spinal-cord DEMON_ORGAN_SPINAL_CORD
+                        The amount of spinal cord demon organs you want your characters to have
+  --demon-organ-teeth DEMON_ORGAN_TEETH
+                        The amount of teeth demon organs you want your characters to have
 ```
 
 Credits:
