fe8ffeb140f9b3ed7108f769c4b289b1ee51f1a6
unixtreme <unixtreme@gmail.com>
2018-11-16 10:18:18 +0000

    Implements save manager to handle Save Data. Implements all currencies. Adds subparser for currencies.

---
+++ save_manager/__init__.py
@@ -0,0 +1,68 @@
+import Account_pb2
+
+from save_manager import save_handler
+from settings import CURRENCY_LIST
+from struct import pack
+
+
+class SaveData(object):
+    def __init__(self, account_file, output_file):
+        self.account_file = account_file
+        self.output_file = output_file
+        self.account_file = account_file
+        # load encrypted save data
+        self.account_enc = save_handler.load_encrypted_file(self.account_file)
+        self.account_dec = save_handler.decrypt_save(self.account_enc)
+        # load saved definition
+        self.asd = Account_pb2.SavedDefinition()
+        # parse decrypted save definitions TODO: Should this be done here?
+        self.asd.ParseFromString(self.account_dec)
+        # load currency information
+        self.currency_names = self.load_currencies()
+        self.last_played_hero_id = save_handler.hexlify(pack(">Q", self.asd.digest.last_played_hero_id.id_low))
+        # load last played hero
+        self.heroes = {}
+        self.heroes[self.last_played_hero_id] = save_handler.load_hero(self.last_played_hero_id)
+
+    def load_currencies(self):
+        """
+        Loads currencies from the account file.
+        """
+        currency_names = {}
+        for partition in self.asd.partitions:
+            currencies = partition.currency_data.currency
+            for currency in currencies:
+                currency_id = str(currency.id)
+                if currency_id in CURRENCY_LIST:
+                    currency_name = CURRENCY_LIST[currency_id]
+                    currency_names[currency_name.lower().replace(' ', '-')] = currency_id
+                    print("Loaded %s: %s" % (currency_name, currency.count))
+                else:
+                    print("Unknown currency ID: %s" % currency.id)
+        return currency_names
+
+    def set_currency(self, currency_id, amount):
+        """
+
+        :param currency_id: int() id of the currency to modify
+        :param amount: int() amount to set the currency to
+        :return:
+        """
+        currency_id = int(currency_id)
+        if currency_id in list(self.currency_names.values()):
+            for i in range(len(self.asd.partitions)):
+                if len(self.asd.partitions[i].currency_data.currency) > 0:
+                    current_currency = self.asd.partitions[i].currency_data.currency[currency_id]
+                    current_currency.count = amount
+                    print("Set currency {0} to {1}".format(self.currency_names[str(currency_id)], current_currency.count))
+        else:
+            print("Currency not found on the account, this normally means you need to play and collect some currency first.")
+
+    def commit_all_changes(self):
+        # TODO: perhaps automatically backup account.dat
+        # serialize and encrypt account file
+        account_mod_dec = self.asd.SerializeToString()
+        account_mod_enc = save_handler.encrypt_save(account_mod_dec)
+        # commit account file to storage only if it changed
+        if self.output_file and (account_mod_enc != self.account_enc):
+            save_handler.commit_to_file(account_mod_enc, self.output_file)
---
+++ save_manager/save_handler.py
@@ -0,0 +1,103 @@
+import Hero_pb2
+from os.path import isfile, isdir, join
+from settings import HERO_DIR, AFFIX_LIST
+from sys import getsizeof
+from binascii import hexlify as _hexlify
+
+XOR_KEY = 0x305F92D82EC9A01B
+BYTE_MAX_VALUE = 255
+
+
+def truncate(num: int, boundary: int, signed: bool, endian: str = "little") -> int:
+    """
+    Truncate an int to a given byte boundary
+    :param num: the number to truncate
+    :param boundary: the byte boundary
+    :param signed: whether or not the int is signed
+    :param endian: the integer's endianness
+    :return: the truncated integer
+    """
+    return int.from_bytes(num.to_bytes(getsizeof(num), endian, signed=signed)[:boundary], endian, signed=signed)
+
+
+def decrypt_save(data: (bytes, bytearray)) -> (bytes, bytearray):
+    """
+    Decrypt a save file
+    :param data: the save data to decrypt
+    :return: the decrypted save data
+    """
+
+    global XOR_KEY
+
+    if isinstance(data, bytes):
+        data = bytearray(data)
+
+    num = XOR_KEY
+    for i in range(len(data)):
+        data[i] ^= (num & BYTE_MAX_VALUE)
+        num = truncate((((num ^ data[i]) << 56) | num >> 8), 8, False)
+
+    return bytes(data)
+
+
+def encrypt_save(data: (bytes, bytearray)) -> (bytes, bytearray):
+    """
+    Encrypt a save file
+    :param data: the save data to encrypt
+    :return: the encrypted save data
+    """
+    global XOR_KEY
+
+    if isinstance(data, bytes):
+        data = bytearray(data)
+
+    num1 = XOR_KEY
+    for i in range(len(data)):
+        num2 = data[i]
+        data[i] ^= (num1 & BYTE_MAX_VALUE)
+        num1 = truncate(((num1 ^ num2) << 56) | num1 >> 8, 8, False)
+
+    return bytes(data)
+
+
+def hexlify(b: (bytes, bytearray)) -> (bytes, bytearray):
+    """
+    bytes -> hex
+    :param b: the bytes you want to convert to hex
+    :return: the hex string representation of the specified bytes
+    """
+    return _hexlify(b).decode("utf8")
+
+
+def load_encrypted_file(encrypted_file):
+    # TODO: Unify account and hero loading
+    assert isfile(encrypted_file), "File {} doesn't exit".format(encrypted_file)
+    with open(encrypted_file, 'rb') as f:
+        file_enc = f.read()
+    return file_enc
+
+
+def load_hero(hero_id):
+    # hero ID is in big endian for god knows why (maybe PS3 and Xbox 360 releases?)
+    if isdir(join("saves", "Modded", HERO_DIR)):
+        last_hero_save_path = join("saves", "Modded", HERO_DIR, hero_id + ".dat")
+        if isfile(last_hero_save_path):
+            with open(last_hero_save_path, "rb") as f:
+                hero_enc = f.read()
+            hero_dec = decrypt_save(hero_enc)
+            return hero_dec
+"""
+# print(hsd.items.items)
+for single in hsd.items.items:
+    for affix in single.generator.base_affixes:
+        if str(affix) in AFFIX_LIST:
+            print(AFFIX_LIST[str(affix)])
+        else:
+            print("Unknown Affix: %s" % (affix))
+"""
+
+
+def commit_to_file(data, target):
+    with open(target, "wb") as f:
+        f.write(data)
+    return "Changed written to {}".format(target)
